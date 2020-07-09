818b7f93cff4a2fbd7e2d517acc67b50751264b4
marekpinto <marek.pinto@gmail.com>
2020-07-09 14:40:43 -0700

    Made some changes, but  problems with react-bootstrap

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 import { logIn } from "../actions/authActions";
 
@@ -14,13 +16,18 @@ const Login = ({ logInConnect }: IProps ) => (
               <h1>Login</h1>
               </div>
             <div className="form-group">
-                <input type="text" className="form-control item" id="username" placeholder="Username" />
+                <input type="text" className="form-control item" id="email" placeholder="Email Address" />
             </div>
             <div className="form-group">
                 <input type="password" className="form-control item" id="password" placeholder="Password" />
             </div>
             <div className="form-group">
-                <button type="button" className="btn btn-block create-account" onClick={logInConnect}>Login</button>
+            <Container>
+            <Row>
+                <Col><button type="button" className="btn btn-block create-account" onClick={logInConnect}>Login</button></Col>
+                  <Col><button type="button" className="btn btn-block create-account"><Link to ='/register'>Sign Up</Link></button></Col>
+            </Row>
+            </Container>
             </div>
         </form>
                 <div className="register">
@@ -28,7 +35,7 @@ const Login = ({ logInConnect }: IProps ) => (
           </div>
 
         <div className="social-media">
-            <h5>Sign in with social media</h5>
+            <h5>Sign in with social media (Take out if we don't use)</h5>
             <div className="social-icons">
                 <a href="#"><i className="icon-social-facebook" title="Facebook"></i></a>
                 <a href="#"><i className="icon-social-google" title="Google"></i></a>
