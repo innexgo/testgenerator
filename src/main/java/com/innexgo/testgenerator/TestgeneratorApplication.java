145dfcc2e245badb8df809efae32976abcb7e113
gpi <gpimpale29@gmail.com>
2020-07-19 12:15:01 -0700

    new structure

---
+++ src/main/java/com/innexgo/testgenerator/TestgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.innexgo.testgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgeneratorApplication.class, args);
+	}
+
+}
